2570f2afd7a3b856db1071f4c825395281655d0f
hemanthrachala <hemanthrachala@gmail.com>
2020-07-15 22:23:01 +0530

    Jul 15  22:23:01

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -12,7 +12,7 @@ class Queue:
         self.storage.append(new_element)
 
     def peek(self):
-        return self.storage(0)
+        return self.storage[0]
 
     def dequeue(self):
         return self.storage.pop(0)
