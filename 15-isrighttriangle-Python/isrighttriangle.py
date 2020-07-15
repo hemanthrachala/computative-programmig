2661087cb369daccbd53a1247d6e953f0c146d85
hemanthrachala <hemanthrachala@gmail.com>
2020-07-15 12:16:08 +0530

    Jul 15  12:16:07

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,7 +7,20 @@
 import math
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	return 0
+	l = distance(x1,y1,x2,y2)
+	m = distance(x2,y2,x3,y3)
+	n = distance(x1,y1,x3,y3)
+
+	maximum = max(l,m,n)
+
+	if(maximum == l and math.isclose(l*l,(m*m + n*n))):
+		return True
+	if(maximum == m and math.isclose(m*m,(l*l + n*n))):
+		return True
+	if(maximum == n and math.isclose(n*n,(l*l + m*m))):
+		return True	
+	else:
+		return False		
 	
 def distance(x1,y1,x2,y2):
 
