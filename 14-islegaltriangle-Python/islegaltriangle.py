1a3151e3324a2019c42cf806aba10fc9cccec2e6
hemanthrachala <hemanthrachala@gmail.com>
2020-07-15 13:38:48 +0530

    Jul 15  13:38:48

--- 14-islegaltriangle-Python/islegaltriangle.py
+++ 14-islegaltriangle-Python/islegaltriangle.py
@@ -7,8 +7,10 @@
 
 def islegaltriangle(s1, s2, s3):
 	# your code goes here
-	if((s1+s2>s3) and (s1+s3>s2) and (s2+s3)>sq):
+	if((s1+s2>s3) and (s1+s3>s2) and (s2+s3)>s1):
 		return True
+	elif(float(s1+s2>s3) and (s1+s3>s2) and (s2+s3>s1)):
+		return True	
 	else:
 		return True
 
