8cd1165a43c5096eba8c3539686a7dd905755dc0
hemanthrachala <hemanthrachala@gmail.com>
2020-07-16 12:07:05 +0530

    done

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -15,5 +15,13 @@ def fun_get_kth_digit(digit, k):
 			return int(n[len(n)-k-1])
 		else:
 			return 0
-	else:
-		return 0			
+	elif(n < 0):
+		n = -1 *n
+		n = str(n)
+		if( k < len(n)):
+			return int(n[len(n)-k-1])
+		else:
+			return 0	
+
+
+					
