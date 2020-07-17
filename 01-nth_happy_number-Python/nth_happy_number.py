a9e5bff922ab4fe843852eabbf0f41359585127f
hemanthrachala <hemanthrachala@gmail.com>
2020-07-17 22:15:32 +0530

    Jul 17  22:15:32

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -23,14 +23,17 @@ def isHappyNumber(n):
 		return sum
 	l = []
 
-	if squares_sum(n) not in l:
-		res = squares_sum(res)		 
-
-
-
-
+	while squares_sum(n) not in l:
+		res = squares_sum(res)
+		if res == 1:
+			return True
+		else:
+			l.append(res)
+			n = res			 
 
 
 
 def fun_nth_happy_number(n):
-	return 0
+	
+	happynum = [i for i in range(10) if isHappyNumber(i)]
+	return happynum[n]
