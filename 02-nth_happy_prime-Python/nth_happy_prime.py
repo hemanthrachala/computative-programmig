9043cc15e25c93da2d421b8cf76226522e10b71e
hemanthrachala <hemanthrachala@gmail.com>
2020-07-18 20:00:43 +0530

    Jul 18  20:00:43

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -3,6 +3,39 @@
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
 
 
+def isHappyNumber(n):
+
+	def squares_sum(num):
+		sum = 0
+
+		while ( num > 0):
+			rem = num % 10
+			sum = sum + (rem*rem)
+			num = num // 10
+		return sum
+	l = []
+
+	while squares_sum(n) not in l:
+		res = squares_sum(n)
+		if res == 1:
+			return True
+		else:
+			l.append(res)
+			n = res			 
+	else:
+		return False
+
+def is_prime(num):
+	if num >= 1 :
+		for i in range(2,num):
+			if (num % i == 0):
+				return False
+		return True
+	else:
+		return False			
+
 
 def fun_nth_happy_prime(n):
-	return 0
+	
+	l = [i for i in range(100) if (isHappyNumber(i) and isprime(i))]
+	return l.__getitem__(n)
