11b10303896f68dbc060f258d0cbf9368b3c6457
hemanthrachala <hemanthrachala@gmail.com>
2020-08-06 20:36:03 +0530

    Aug 06  20:36:03

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,8 +8,20 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
+
+def is_even(n):
+	if n == 0:
+		return 0
+	else:
+
+		t= n % 10 
+		if n % 2 == 0:
+			return is_even(n//10) + temp	
+		else:
+			return is_even(n//10)	
+
 def fun_recursion_onlyevendigits(l): 
 	if not l:
 		return []
 	else:
-		return [is_even()] + fun_recursion_onlyevendigits()	
+		return [is_even(0)] + fun_recursion_onlyevendigits(l[1:])	
